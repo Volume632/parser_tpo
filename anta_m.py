7c99eefd37bed73a318bbceba3280ee5ad9b0519
frost_x <zakgleb@gmail.com>
2024-03-17 23:22:06 +0300

    3

---
+++ anta_m.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://antamedia.by/elektronika/elektroinstrumenty/perforatory/"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "lxml")
+
+data = soup.find("div", attrs={"id": "content"})
+items = data.find("div", class_="row-flex row-price category-page")
+#items = data.find_all("div", attrs={"class":"product-layout"})
+#content > div.row-flex.row-price.category-page > div:nth-child(1)
+
+name = data.find("div", class_="product-name").text
+
+price = soup.find(class_="price_no_format").text
+
+#url_amg = data.find(class_="pictureSlider").get("picture")
+
+print(name, price)
