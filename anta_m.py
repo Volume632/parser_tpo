0008b896e8b1fb79693c63ad59510da400ce31f7
frost_x <zakgleb@gmail.com>
2024-03-17 23:38:10 +0300

    4

--- anta_m.py
+++ anta_m.py
@@ -16,6 +16,6 @@ name = data.find("div", class_="product-name").text
 
 price = soup.find(class_="price_no_format").text
 
-#url_amg = data.find(class_="pictureSlider").get("picture")
+url_amg = data.find("img").get("src")
 
-print(name, price)
+print(name + "\n" + price + "\n" + url_amg + "\n\n")
