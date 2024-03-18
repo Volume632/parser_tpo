19797598a62b02aebb7df699c80377ad87e86ecb
frost_x <zakgleb@gmail.com>
2024-03-19 01:29:11 +0300

    10

--- anta_m.py
+++ anta_m.py
@@ -1,21 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+#for count in range(1, 14):
 url = "https://antamedia.by/elektronika/elektroinstrumenty/perforatory/"
 
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "lxml")
+data = soup.find_all("div", attrs={"class": "row-flex row-price category-page"})
 
-data = soup.find("div", attrs={"id": "content"})
-items = data.find("div", class_="row-flex row-price category-page")
-#items = data.find_all("div", attrs={"class":"product-layout"})
-#content > div.row-flex.row-price.category-page > div:nth-child(1)
+for i in data:
+    name = i.find("div", class_="product-name").text
+    url_amg = i.find("img").get("src")
+    price = i.find("div", class_="price").text
 
-name = data.find("div", class_="product-name").text
-
-price = soup.find(class_="price_no_format").text
-
-url_amg = data.find("img").get("src")
-
-print(name + "\n" + price + "\n" + url_amg + "\n\n")
+print(name + price + url_amg)
